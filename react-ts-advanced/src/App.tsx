63155a7701fccce3f42ff281f666204c4f9d2b7a
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-04-15 19:44:44 +0200

    type predicates components

--- react-ts-advanced/src/App.tsx
+++ react-ts-advanced/src/App.tsx
@@ -1,10 +1,17 @@
 import Input from "./components/Input";
+import Button from "./Button2";
 
 function App() {
   return (
     <main>
       <Input id="name" label="Your Name:" type="text" />
       <Input id="email" label="Your Emal:" type="text" disabled />
+      <p>
+        <Button>Submit</Button>
+      </p>
+      <p>
+        <Button href="https://www.google.com/" target="_blank">Google</Button>
+      </p>
     </main>
   );
 }
