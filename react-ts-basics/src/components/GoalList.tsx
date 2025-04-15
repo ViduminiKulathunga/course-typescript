4b5a13eb7d5af4bbca3f8585857c7bf9081c805c
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-04-15 16:42:14 +0200

    typescript discriminated unions

--- react-ts-basics/src/components/GoalList.tsx
+++ react-ts-basics/src/components/GoalList.tsx
@@ -1,5 +1,7 @@
 import Goal from "./Goal";
 import { type Goal as GoalType } from "../App";
+import { InfoBox } from "./InfoBox";
+import { ReactNode } from "react";
 
 type GoalItem = {
   goal: GoalType[];
@@ -7,15 +9,31 @@ type GoalItem = {
 };
 
 export function GoalList({ goal, onDeleteGoal }: GoalItem) {
+  if (goal.length === 0) {
+    return <InfoBox mode="hint">You have no golas. Start adding some!</InfoBox>;
+  }
+
+  let warningBox: ReactNode;
+  if (goal.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="medium">
+        You are collected a lot of goals. Don'y put too much on your plate.
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goal.map((goalIem) => (
-        <li key={goalIem.id}>
-          <Goal id={goalIem.id} title={goalIem.title} onDelete={onDeleteGoal}>
-            <p>{goalIem.description}</p>
-          </Goal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goal.map((goalIem) => (
+          <li key={goalIem.id}>
+            <Goal id={goalIem.id} title={goalIem.title} onDelete={onDeleteGoal}>
+              <p>{goalIem.description}</p>
+            </Goal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
